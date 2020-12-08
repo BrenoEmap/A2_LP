89c5a22538181d6733e07b70428419087a774f9e
atomazf <a.tomazf@gmail.com>
2020-12-08 11:47:14 -0300

    Renomeação de pasta: connectdb+data+unittest

---
+++ connectdb+data+unittest/teste unitario UFC.py
@@ -0,0 +1,8 @@
+import unittest
+from math import pi
+from dataUFC import df
+
+class TesteUFC(unittest.Testcase):
+    def test_thingy(self):
+        
+        self.assertEqual(df,data_ufc)
--- teste unitario UFC.py
+++
@@ -1,8 +0,0 @@
-import unittest
-from math import pi
-from datapolish import df
-
-class TesteUFC(unittest.Testcase):
-    def test_thingy(self):
-        
-        self.assertEqual(df,data_ufc)
